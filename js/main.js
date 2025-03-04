0398fd77a3d62cd0ccf6eb08bd141fa28c8d6b2e
Rosa-Sot12 <18390846@uagro.mx>
2025-03-04 11:37:15 -0600

    Subiendo ejercicio de metodo push

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let estudiantes = [
+    {
+    nombre: 'Alvaro',
+    promedio : 9,
+    curso : 'Redes',
+    },
+    {
+    nombre: 'Daniel',
+    promedio : 6,
+    curso : 'Bases de Datos',
+    },
+    {
+    nombre: 'Alexis',
+    promedio : 3,
+    curso : 'Arquitectura de computadoras',
+    },
+    ]
+
+estudiantes.push({nombre: "Josué",
+    promedio: 9,
+    curso: "Redes"},{nombre: "Gerardo",
+    promedio: 9,
+    curso: "Principios de Programación I" })
+console.log(estudiantes)
